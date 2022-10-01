6ed44887803a4ba4922df8a7041dff3f0cea22f6
Mahmoud Qandeel <99659529+Qandeeil@users.noreply.github.com>
2022-10-01 23:38:35 +0300

    Update Connection.js

--- API/Connection.js
+++ API/Connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Qandeeil:sIczRz1AWkKHX45B@cluster0.wqfwxbi.mongodb.net/UploadImages', {useNewUrlParser: true});
+mongoose.connect('*******************************', {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
